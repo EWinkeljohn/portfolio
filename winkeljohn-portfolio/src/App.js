fff4e278aee1c05a19a290e94d1182980ed97b3b
EWinkeljohn <emcwink@gmail.com>
2023-11-30 21:38:53 -0800

    added image, working links, color pallette

--- winkeljohn-portfolio/src/App.js
+++ winkeljohn-portfolio/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
+import windmill_banner from './components/windmill_banner.webp';
 import './App.css'
 
 const App = () => {
   return (
     <>
      <Header/>
+     <br/>
+     <img src={windmill_banner} height="300px" alt=""/>
     </>
   )
 }
--- winkeljohn-portfolio/src/components/Header.js
+++ winkeljohn-portfolio/src/components/Header.js
@@ -1,22 +1,29 @@
 import React from "react"
+import { DiGithub } from "react-icons/di";
 
 const Header = () => {
     return (
     <>
     <div className="header">
         <div className="git-link">
-            <button> <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" style= {{ width: 35, height: 35 }} />
-            </button>
-
+            <a href="https://github.com/EWinkeljohn">
+            <button> <DiGithub/> </button>
+            </a>
         </div>
-            <div className="linkedin-link">
-            <button> <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Linkedin_Shiny_Icon.svg" style= {{ width: 35, height: 35 }} />
+        <div className="linkedin-link">
+            <a href="https://www.linkedin.com/in/ewinkeljohn/">
+            <button> <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Linkedin_Shiny_Icon.svg" style= {{ width: 35, height: 35 }} alt=""/>
             </button>
+            </a>
         </div>
-
-        Hey There! Welcome to Emery's Portfolio
-
-        <button className="about-me">More About Emery!</button>
+        <div>
+            <h2> Welcome to Emery Winkeljohn's Portfolio! </h2>
+            <p> Full-Stack Software Developer | Sustainability Enthusiast | Career Switcher </p>
+        </div>
+        <div className="right-buttons">
+            <button className="about-me">More About Emery</button>
+            <button className="project-button">Projects</button>
+        </div>
     </div>
     </>
     )   
